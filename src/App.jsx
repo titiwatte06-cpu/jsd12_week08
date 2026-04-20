7b171d6a25b482e8bd40d7b7e002b02c4865a65f
Titiwat <titiwat.te06@gmail.com>
2026-04-20 15:13:49 +0700

    week08

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
+import Castle from "./components/Castle";
+
 export default function App() {
   return (
-    <div className="min-h-screen flex justify-center bg-blue-950">
-      <div className="p-6 gap-y-6 flex flex-col justify-start w-[80%] lg:w-[70%]">
-        <h1 className="w-full p-6 bg-amber-100 font-extrabold">
-          React App Starter
-        </h1>
-        <section className="w-full p-5 bg-amber-100 flex">
-          <ul className="list-inside list-disc flex-1">
-            <span className="font-semibold">Tech Stack:</span>
-            <li>Vite</li>
-            <li>React</li>
-            <li>JavaScript</li>
-            <li>Tailwind</li>
-          </ul>
-        </section>
-      </div>
+    <div className="pb-80 py-10 gap-y-4 flex flex-col justify-center items-center min-h-screen bg-blue-300">
+      <p className="text-purple-400">
+        Message for JSD12:
+
+        <span className="text-yellow-300">
+          {/* question or waiting for a message */}
+        </span>
+
+      </p>
+      <textarea value="banana" onChange="" className="bg-white" placeholder="Type your message here ..."/>
+      <p className="text-green-300">Reply from Secret Room
+        <span className="text-yellow-300">{/* answer for a waiting reply */}</span>
+      </p>
+      <Castle />
     </div>
-  );
+  ) 
 }
